97ea07c67646105417fcaec91b6ee87976e0444b
Randale Liwanag <rliwanag@ucsd.edu>
2021-02-25 13:07:01 -0800

    Lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'KDOYzdVe0PdqmMCjYT0f4HGWtGm6dtHn8EY';
+
+	// 'map' refers to a <div> element with the ID map	
+	var map = L.mapquest.map('map', {
+		center: [32.87874, -117.23592],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.87874, -117.23592]).addTo(map);
 }
+
